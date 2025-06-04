316e6a79e4477e5e5e2cddffc0b72616d552bed2
Luigi Bittencorut Marzinotto <bittencourt.marzinotto@gmail.com>
2025-06-03 21:30:10 -0300

    create program for IMC test

---
+++ CalcularIMC/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CalcularIMC;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe o peso em kg: ");
+        double peso = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe sua altura em metros: ");
+        double altura = double.Parse(Console.ReadLine());
+
+        double resultIMC = peso / (altura * altura);
+
+        if (resultIMC < 20)
+        {
+            Console.WriteLine("\nVocê esta abaixo do peso, seu IMC " + resultIMC);
+        }
+        else if (resultIMC >= 20 && resultIMC <= 24)
+        {
+            Console.WriteLine("\nVocê pesa normal, seu IMC " + resultIMC);
+        }
+        else if (resultIMC >= 25 && resultIMC <= 29)
+        {
+            Console.WriteLine("\nVocê esta acima do peso, seu IMC " + resultIMC);
+        }
+        else if (resultIMC >= 30 && resultIMC <= 34)
+        {
+            Console.WriteLine("\nVocê esta acima obeso, seu IMC " + resultIMC);
+        }
+        else
+        {
+            Console.WriteLine("\nVocê esta parecendo plutão, seu IMC " + resultIMC);
+        }
+    }
+}
---
+++ Condicoes/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Codicoes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero = 10;
+				
+				string result = (numero > 5)? "Número maior q 5" : "Número menor q 5";
+        Console.WriteLine(result);
+    }
+}
