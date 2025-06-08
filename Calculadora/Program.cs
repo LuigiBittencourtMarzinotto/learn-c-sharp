f3a01c5d4711d211dbbee4c67fc99d6fcbdeec88
Luigi Bittencorut Marzinotto <bittencourt.marzinotto@gmail.com>
2025-06-08 19:23:44 -0300

    make calculadora test by course

---
+++ Calculadora/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Calculadora;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Digite o primeiro valor: ");
+        double val1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo valor: ");
+        double val2 = double.Parse(Console.ReadLine());
+        
+        Operacao:
+        Console.Write("Digite a operação: ");
+        char op = char.Parse(Console.ReadLine());
+        double result = 0;
+        switch (op)
+        {
+            case '*':
+                result = val1 * val2;
+                break;
+
+            case '/':
+                result = val1 / val2;
+                break;
+
+            case '+':
+                result = val1 + val2;
+                break;
+
+            case '-':
+                result = val1 - val2;
+                break;
+
+            default:
+                Console.WriteLine("Operação não encontrada, informe novamente");
+                goto Operacao;
+        }
+
+        Console.WriteLine("O resultado da operação deu: " + result);
+    }
+}
---
+++ Controle/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Controle;
+
+class Controle
+{
+    public static void Main(String[] args)
+    {
+        Inicio:
+        Console.Write("Escolha uma numero:");
+        int escolha = int.Parse(Console.ReadLine());
+        
+        // Label, quando colocamos na frente de uma frase um ":" significa que ela é um label, e podemos usar ela 
+        // como endpoint para volta nela se quisermos
+        switch (escolha)
+        {
+            case 0:
+                Console.WriteLine("Escolha 0");
+                break;
+
+            case 1:
+                Console.WriteLine("Escolha 1");
+                break;
+
+            case 2:
+                Console.WriteLine("Escolha 2");
+                goto case 1;
+                // Tabém podemos fazer ele ir para outro case mesmo que o valor da variavel assistida pelo switch nao correponder
+            default:
+                Console.WriteLine("Escolha dnv");
+                // o goto usamos como um modo de volta a algo ou entra, por exemplo
+                // No caso a baixo fazemos ele volta a label inicio sendo o começo do nosso projeto
+                goto Inicio;
+                break;
+        }
+    }
+}
