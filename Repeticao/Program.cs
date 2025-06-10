ec9249ba765503615e34160d0ed440ae0cc84393
Luigi Bittencorut Marzinotto <bittencourt.marzinotto@gmail.com>
2025-06-09 22:00:10 -0300

    fibonacci test

---
+++ Repeticao/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace repeticao;
+
+class Repeticao
+{
+    public static void Main(string[] args)
+    {
+        // Laço de repetição while 
+        int value = 0;
+        do
+        {
+            Console.WriteLine("Dentro do laço " + value);
+            value++;
+        }
+        while (value < 10);
+    
+        
+        Console.WriteLine("Fora do laço");
+    }
+}
---
+++ fibonnaci/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace fibonnaci;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("################# FIBONACCI #################");
+        Console.Write("Escolha um número: ");
+        int value = int.Parse(Console.ReadLine());
+        int soma = 0;
+        int[] arrayValues = new int[value];
+        arrayValues[0] = 0;
+        arrayValues[1] = 1;
+
+        for (int i = 2; i < value; i++)
+        {
+            arrayValues[i] = arrayValues[i - 1] + arrayValues[i - 2];
+        }
+        Console.WriteLine("O valor é " + arrayValues[value - 1]);
+    }
+}
